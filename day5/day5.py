99d150e680143c231c72d1023d56a913fccb9194
Bas Jansen <basj9559@gmail.com>
2025-12-05 06:59:53 +0100

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,49 @@
+import copy
+
+
+def main():
+    input = open('input').read().split('\n\n')
+    ranges = input[0].split('\n')
+    ingredients = input[1].split('\n')
+    res = 0
+    freshness = []
+    for r in ranges:
+        freshness += [list(map(int,r.split('-')))]
+    for ingredient in ingredients:
+        for fresh in freshness:
+            if int(ingredient) >= fresh[0] and int(ingredient) <= fresh[1]:
+                res += 1
+                break
+
+    return res
+
+def mergeRanges(result):
+    result.sort(key=lambda x: x[0])
+    merged = [result[0]]
+
+    for start, end in result[1:]:
+        last_start, last_end = merged[-1]
+
+        if start <= last_end:
+            merged[-1][1] = max(last_end, end)
+        else:
+            merged.append([start, end])
+    return merged
+
+def main2():
+    ranges = open('input').read().split('\n\n')[0].split('\n')
+    result = []
+    for r in ranges:
+        result.append(list(map(int, r.split('-'))))
+    while(True):
+        old_result = copy.deepcopy(result)
+        result = mergeRanges(result)
+        if(old_result == result):
+            break
+    res = 0
+    for i in result:
+        res += i[1]-i[0]+1
+    return res
+
+print(main())
+print(main2())
